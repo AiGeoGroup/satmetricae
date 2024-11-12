5002b0c8cf5e170296e9f95388f0d4c0b267f457
Yong Sun <157693421+AiGeoGroup@users.noreply.github.com>
2024-11-12 10:45:26 +0800

    Update tripletaeloss.py

--- tripletaeloss.py
+++ tripletaeloss.py
@@ -12,8 +12,9 @@ class TripletAELoss(nn.Module):
         return (x1-x2).pow(2).sum(1)
 
     def forward(self, anchor, positive, negative, anchor_img, pred_img, gamma=0.01):
+        criterion = nn.MSELoss()
         distance_positive = self.cal_eucliden(anchor, positive)
         distance_negative = self.cal_eucliden(anchor, negative)
-        contruct_loss = self.cal_eucliden(anchor_img, pred_img)
+        contruct_loss = criterion(anchor_img, pred_img)
         losses = torch.relu(distance_positive - distance_negative + self.margin) + gamma * contruct_loss
         return losses.mean()
