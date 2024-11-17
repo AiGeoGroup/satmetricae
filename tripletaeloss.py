af990fcfc752c59198a018a536573ee111ba7bbd
Yong Sun <157693421+AiGeoGroup@users.noreply.github.com>
2024-11-17 09:16:46 +0800

    Update tripletaeloss.py

--- tripletaeloss.py
+++ tripletaeloss.py
@@ -11,7 +11,7 @@ class TripletAELoss(nn.Module):
     def cal_eucliden(self, x1, x2):
         return (x1-x2).pow(2).sum(1)
 
-    def forward(self, anchor, positive, negative, anchor_img, pred_img, gamma=0.5):
+    def forward(self, anchor, positive, negative, anchor_img, pred_img, gamma=0.9):
         criterion = nn.MSELoss()
         distance_positive = self.cal_eucliden(anchor, positive)
         distance_negative = self.cal_eucliden(anchor, negative)
